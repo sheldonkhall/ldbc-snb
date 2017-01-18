702b006963f5467a51f7c7a47f04a91e7fbb53b9
Sheldon <sheldon@mindmaps.io>
2017-01-18 18:00:35 +0000

    added simple ontology and a few lines of the serialiser
    Conflicts:
    	params.ini
    	pom.xml
    	run.sh

--- run.sh
+++ run.sh
@@ -20,9 +20,9 @@ export LDBC_SNB_DATAGEN_HOME
 
 cd $SCRIPTPATH
 mvn clean
-mvn -DskipTests assembly:assembly
+mvn -DskipTests assembly:assembly 
 
-# This is needed since MacOS by default is case-insensitive!
+// hack because osx by default is not case sensitive
 zip -d $LDBC_SNB_DATAGEN_HOME/target/ldbc_snb_datagen-0.2.5-jar-with-dependencies.jar META-INF/LICENSE
 
 $HADOOP_HOME/bin/hadoop jar $LDBC_SNB_DATAGEN_HOME/target/ldbc_snb_datagen-0.2.5-jar-with-dependencies.jar $LDBC_SNB_DATAGEN_HOME/params.ini
