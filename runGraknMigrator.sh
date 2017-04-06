6ecfd412b7019784d7373a092ae0e6e7e0641402
sheldonkhall <sheldonkhall@users.noreply.github.com>
2017-04-06 11:36:39 +0100

    new full SNB ontology (#25)
    * migrator change
    
    * resource ontology file done
    
    * entity ontology file half done
    
    * ontology done
    
    * update small csv migrators
    
    * update to full ontology
    
    * update to full ontology
    
    * tidy up unused graql and rest scripts
    
    * started writing new templates

--- runGraknMigrator.sh
+++ runGraknMigrator.sh
@@ -6,34 +6,36 @@ DATA=$SCRIPTPATH/./social_network
 GRAQL=$SCRIPTPATH/./graql
 
 # generate CSV files
-$SCRIPTPATH/run.sh
+#$SCRIPTPATH/run.sh
 
 # load ontology
-graql.sh -k $2 -f $GRAQL/snb-ontology-simple.gql -r $1
+graql.sh -k $2 -f $GRAQL/ldbc-snb-1-resources.gql -r $1
+graql.sh -k $2 -f $GRAQL/ldbc-snb-2-relations.gql -r $1
+graql.sh -k $2 -f $GRAQL/ldbc-snb-3-entities.gql -r $1
 
 # load data
-time migration.sh csv -s \| -t $GRAQL/template-user.gql -i $DATA/user_0_0.csv -k $2 -u $1
-tail -n +2 $DATA/user_0_0.csv | wc -l
+time migration.sh csv -s \| -t $GRAQL/template-person.gql -i $DATA/person_0_0.csv -k $2 -u $1
+tail -n +2 $DATA/person_0_0.csv | wc -l
 
-time migration.sh csv -s \| -t $GRAQL/template-message.gql -i $DATA/message_0_0.csv -k $2 -u $1
-tail -n +2 $DATA/message_0_0.csv | wc -l
+time migration.sh csv -s \| -t $GRAQL/template-comment.gql -i $DATA/comment_0_0.csv -k $2 -u $1
+tail -n +2 $DATA/comment_0_0.csv | wc -l
 
-time migration.sh csv -s \| -t $GRAQL/template-tag.gql -i $DATA/tag_0_0.csv -k $2 -u $1
-tail -n +2 $DATA/tag_0_0.csv | wc -l
+#time migration.sh csv -s \| -t $GRAQL/template-tag.gql -i $DATA/tag_0_0.csv -k $2 -u $1
+#tail -n +2 $DATA/tag_0_0.csv | wc -l
 
-sed -i '' "1s/Message/Comment/" $DATA/message_replyOf_message_0_0.csv
-time migration.sh csv -s \| -t $GRAQL/template-message_replyOf_message.gql -i $DATA/message_replyOf_message_0_0.csv -k $2 -u $1
-tail -n +2 $DATA/message_replyOf_message_0_0.csv | wc -l
+#sed -i '' "1s/Message/Comment/" $DATA/message_replyOf_message_0_0.csv
+#time migration.sh csv -s \| -t $GRAQL/template-message_replyOf_message.gql -i $DATA/message_replyOf_message_0_0.csv -k $2 -u $1
+#tail -n +2 $DATA/message_replyOf_message_0_0.csv | wc -l
 
-time migration.sh csv -s \| -t $GRAQL/template-message_tags_tag.gql -i $DATA/message_tags_tag_0_0.csv -k $2 -u $1
-tail -n +2 $DATA/message_tags_tag_0_0.csv | wc -l
+#time migration.sh csv -s \| -t $GRAQL/template-message_tags_tag.gql -i $DATA/message_tags_tag_0_0.csv -k $2 -u $1
+#tail -n +2 $DATA/message_tags_tag_0_0.csv | wc -l
 
-sed -i '' "1s/User/User1/" $DATA/user_knows_user_0_0.csv
-time migration.sh csv -s \| -t $GRAQL/template-user_knows_user.gql -i $DATA/user_knows_user_0_0.csv -k $2 -u $1
-tail -n +2 $DATA/user_knows_user_0_0.csv | wc -l
+#sed -i '' "1s/User/User1/" $DATA/user_knows_user_0_0.csv
+#time migration.sh csv -s \| -t $GRAQL/template-user_knows_user.gql -i $DATA/user_knows_user_0_0.csv -k $2 -u $1
+#tail -n +2 $DATA/user_knows_user_0_0.csv | wc -l
 
-time migration.sh csv -s \| -t $GRAQL/template-user_likes_message.gql -i $DATA/user_likes_message_0_0.csv -k $2 -u $1
-tail -n +2 $DATA/user_likes_message_0_0.csv | wc -l
+#time migration.sh csv -s \| -t $GRAQL/template-user_likes_message.gql -i $DATA/user_likes_message_0_0.csv -k $2 -u $1
+#tail -n +2 $DATA/user_likes_message_0_0.csv | wc -l
 
-time migration.sh csv -s \| -t $GRAQL/template-user_writes_message.gql -i $DATA/user_writes_message_0_0.csv -k $2 -u $1
-tail -n +2 $DATA/user_writes_message_0_0.csv | wc -l
+#time migration.sh csv -s \| -t $GRAQL/template-user_writes_message.gql -i $DATA/user_writes_message_0_0.csv -k $2 -u $1
+#tail -n +2 $DATA/user_writes_message_0_0.csv | wc -l
--- runGraknREST.sh
+++ runGraknREST.sh
@@ -20,7 +20,9 @@ export LDBC_SNB_DATAGEN_HOME
 
 # load the ontology to the SNB keyspace
 REMOTE_PATH=`cat params.ini | grep grakn.engine.uri | sed "s/grakn.engine.uri://"`
-graql.sh -k SNB -f $SCRIPTPATH/./graql/snb-ontology-simple.gql -r $REMOTE_PATH
+graql.sh -k SNB -f $SCRIPTPATH/./graql/ldbc-snb-1-resources.gql -r $REMOTE_PATH
+graql.sh -k SNB -f $SCRIPTPATH/./graql/ldbc-snb-2-relations.gql -r $REMOTE_PATH
+graql.sh -k SNB -f $SCRIPTPATH/./graql/ldbc-snb-3-entities.gql -r $REMOTE_PATH
 
 # generate params.ini file that uses Grakn serializers
 cat $LDBC_SNB_DATAGEN_HOME/params.ini | sed 's/small.CSV/grakn.Grakn/' > $TEMP_PARAM_FILE
